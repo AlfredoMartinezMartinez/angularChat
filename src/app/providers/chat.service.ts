936be6a3878ea71bdc43ec53d660447c9536006b
AlfredoMartinezMartinez <alfredo.martinez1@alu.uclm.es>
2018-03-11 23:31:49 +0100

    Comp login y autenticacion google a traves de servicio

--- src/app/providers/chat.service.ts
+++ src/app/providers/chat.service.ts
@@ -3,14 +3,40 @@ import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/fires
 // import { Observable } from 'rxjs/Observable';
 import {Mensaje} from '../interface/mensaje.interface';
 
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase/app';
+
 
 @Injectable()
 export class ChatService {
 
   private itemsCollection: AngularFirestoreCollection<Mensaje>;
   public chats: Mensaje[]=[];
+  public usuario:any= {};
 
-  constructor(private afs: AngularFirestore) { }
+  constructor(private afs: AngularFirestore,
+              public afAuth: AngularFireAuth ) {
+
+      this.afAuth.authState.subscribe( user =>{
+        console.log('Estado del usuario', user);
+
+        if(!user){
+          return;
+        }
+        this.usuario.nombre= user.displayName;
+        this.usuario.uid=user.uid;
+      })
+
+  }
+
+  login(proveedor:string) {
+    this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+    }
+
+
+  logout() {
+    this.afAuth.auth.signOut();
+    }
 
    cargarMensajes(){
      this.itemsCollection = this.afs.collection<Mensaje>('chats',ref=>
