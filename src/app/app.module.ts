f914fd8bac494854fed9054f3e8a87847eb05ce8
AlfredoMartinezMartinez <alfredo.martinez1@alu.uclm.es>
2018-03-11 11:30:42 +0100

    chat y formato

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 import { AngularFireModule } from 'angularfire2';
 import { AngularFirestoreModule } from 'angularfire2/firestore';
@@ -7,6 +8,7 @@ import { AngularFireAuthModule } from 'angularfire2/auth';
 
 import { environment } from '../environments/environment';
 
+//componentes
 import { AppComponent } from './app.component';
 import { ChatComponent } from './components/chat/chat.component';
 
@@ -20,7 +22,8 @@ import { ChatComponent } from './components/chat/chat.component';
     BrowserModule,
     AngularFireModule.initializeApp(environment.firebase), // imports firebase/app needed for everything
     AngularFirestoreModule, // imports firebase/firestore, only needed for database features
-    AngularFireAuthModule, // imports firebase/auth, only needed for auth features,    
+    AngularFireAuthModule, // imports firebase/auth, only needed for auth features,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
